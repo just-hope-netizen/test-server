63a0aa50f9324a8dd7280d6df7809a1d23a25501
just-hope-netizen <hopepeter007@gmail.com>
2022-10-27 23:38:46 +0100

    First commit

--- app.js
+++ app.js
@@ -1,46 +1,25 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
 
-import userRoute from './api/routes/user.js';
 
 const app = express();
 const Port = process.env.PORT || 2000;
 
-dotenv.config();
 
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log('db connected');
-    })
-  .catch((err) => {
-    console.log(err);
-  });
 
 
 const corsOptions = {
-  origin: 'https://test-livid-pi.vercel.app'
-}  
+  origin: '*'
+}
 
 // allow origin request from the url
 app.use(cors(corsOptions));
 
-// parse incoming body payload
-app.use(express.json());
-
-// parse incoming url payload ++++
-app.use(express.urlencoded({extended: true}));
 
 
-//user routes
-app.use('/users', userRoute);
-
-
-app.use((req, res)=>{
-  res.json('wrong endpoint')
+app.get( (req, res) => {
+  res.json({slackUsername: 'hope peter', backend: true, age: 24, bio:'A netizen'})
 });
 
 app.listen(Port, console.log('server started at port 2000')
